54df97008aad355b72704a633e28983509f49848
Guilherme Malicki <guimalicki2002@gmail.com>
2025-08-15 21:35:36 -0300

    feat: Atribuição da classe Pessoa

--- sintaxe-convencoes/Program.cs
+++ sintaxe-convencoes/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using sintaxe_convencoes.models;
+
+Pessoa pessoa = new Pessoa();
+pessoa.Nome = "Guilherme";
+pessoa.Idade = 22;
+pessoa.Apresentar();
--- sintaxe-convencoes/models/Pessoa.cs
+++ sintaxe-convencoes/models/Pessoa.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace models
+namespace sintaxe_convencoes.models
 {
     public class Pessoa
     {
-
         //Propriedades
         public string Nome { get; set; } //get; set; é o padrão de propriedade em C# e serve para encapsular o acesso ao campo.
         public int Idade { get; set; }
 
+        
+
+
         public void Apresentar()
         {
-            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos "); 
+            Console.WriteLine($"Olá, meu nome é " +
+                                $"{ Nome}, e tenho {Idade} anos ");
         }
     }
 }
